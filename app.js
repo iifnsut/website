b1df483a63f4242b8515b5551ebd36d0c13e2d9d
kushagrakiyawat <143614783+kushagrakiyawat@users.noreply.github.com>
2023-12-10 22:44:00 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -692,19 +692,7 @@ const autoPlay = () => {
     timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2500);
 }
 autoPlay();
-// Startup page
-const widgets = document.getElementById('widgets');
-const dropdownSelection = document.getElementById('dropdownSelection');
-function filterWidgets(obj){
-    const divSelector = obj.parentElement;
-    const activeButton = divSelector.getElementsByClassName('active')[0];
-    const activeFilter = activeButton.getAttribute('data-filter');
-    const filter=obj.getAttribute('data-filter');
-    widgets.classList.remove(activeFilter+"-filter");
-    activeButton.classList.remove('active');
-    dropdownSelection.textContent=filter.charAt(0).toUpperCase() + filter.slice(1);
-    widgets.classList.add(filter+'-filter');
-    obj.classList.add('active');
+
 }
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
